e5d65ae4441a2f272098907db5853b2e0790a1a9
thefifthdirectorate <148823531+thefifthdirectorate@users.noreply.github.com>
2023-10-26 23:57:47 -0400

    uh

--- autotyping_lib.py
+++ autotyping_lib.py
@@ -1,31 +1,59 @@
 from pynput.keyboard import Key, Controller
 import time
+import random
+import subprocess
+import pygetwindow
 
 keyboard = Controller()
 
-def Enter(): # Presses Enter
-    keyboard.press(Key.enter)
-    keyboard.release(Key.enter)
+class Globals:
+    ProcessName = ""
+    WindowTitle = ""
 
-def SetDelay(Delay): # Sets delay for each message 
-    time.sleep(Delay)
+class Process:
+    @staticmethod
+    def IsValidWindow():
+        win = pygetwindow.getActiveWindow()
+        return win.title == Globals.WindowTitle
+    
+    @staticmethod
+    def IsValidProcess(): # checks if process exists in taskmanager
+        try:
+            process_list = subprocess.Popen(['tasklist'], stdout=subprocess.PIPE, text=True)
+            for process in process_list.stdout:
+                if Globals.ProcessName in process:
+                    return True
+            return False 
+        except Exception as e:
+            print(f"Error checking processes: {e}")
+            return False
 
-def GetHotkey(HotKey, Do): # Sets your Hotkey for each message
-    with keyboard.pressed(HotKey):
-        Do
+class Functions:
+    @staticmethod
+    def Delay(Delta):
+        time.sleep(Delta)
 
-def SendMessage(string, string1, string2, string3, string4, string5, string6): # Sends messages, you can add more parameters if needed
-    keyboard.type(string)
-    Enter()
-    keyboard.type(string1)
-    Enter()
-    keyboard.type(string2)
-    Enter()
-    keyboard.type(string3)
-    Enter()
-    keyboard.type(string4)
-    Enter()
-    keyboard.type(string5)
-    Enter()
-    keyboard.type(string6)
-    Enter()
+    @staticmethod
+    def SplitMessage(Words):
+        return [char for char in Words]
+    
+    @staticmethod
+    def SendMessage(Message):
+        keyboard.type(Message)
+        keyboard.press(Key.enter)
+        keyboard.release(Key.enter)
+        Functions.Delay(random.uniform(0.08, 0.1))
+
+    @staticmethod
+    def SendRandomMessage(Message): 
+        Functions.SendMessage(random.choice(Message))
+
+    @staticmethod
+    def SendMessageWithinRange(Lowest, Highest, Message):
+        for _ in range(random.randint(Lowest, Highest)): 
+            Functions.SendMessage(Message)
+
+    @staticmethod
+    def SendRandomMessageWithinRange(Lowest, Highest, Message):
+        for _ in range(random.randint(Lowest, Highest)):
+            Functions.SendRandomMessage(Message)
